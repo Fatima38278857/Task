7ed084055f37a7558ca89fa5318bcb3aac4db788
Fatima <aksagovafatima982@gmail.com>
2024-10-13 12:06:14 -0700

    Начало

---
+++ src/test/java/com/example/Manager_Task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Manager_Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
